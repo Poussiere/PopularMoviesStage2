fe1f362dec855a70fef8c7dc54fa43e323076973
Poussiere <von.linnasta@gmail.com>
2017-03-15 14:51:22 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/poussiere/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/poussiere/popularmoviesstage1/MainActivity.java
@@ -367,7 +367,14 @@ public class MainActivity extends AppCompatActivity implements MoviesPostersAdap
     public static int calculateNoOfColumns(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-        int noOfColumns = (int) (dpWidth / 180);
+       
+         int noOfColumns;
+        // If we are on tablet, the posters we'll appeat bigger
+        if (dpWitdth>=600)
+        { noOfColumns = (int) (dpWidth / 375);}
+      
+         else 
+         {noOfColumns = (int) (dpWidth / 180);}
         return noOfColumns;
     }
     @Override
